5b5dd74f72a44c6d8c298bf3f4a417981393c361
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2025-03-15 13:38:51 +0300

    Update blog_sitemap.php

--- upload/catalog/controller/extension/feed/blog_sitemap.php
+++ upload/catalog/controller/extension/feed/blog_sitemap.php
@@ -1,5 +1,5 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2024.
+// *	@copyright	OPENCART.PRO 2011 - 2025.
 // *	@forum		https://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
@@ -24,7 +24,7 @@ class ControllerExtensionFeedBlogSitemap extends Controller {
 					$output .= '<url>';
 					$output .= '<loc>' . $this->url->link('blog/article', 'article_id=' . $article['article_id']) . '</loc>';
 					$output .= '<changefreq>weekly</changefreq>';
-					$output .= '<lastmod>' . date('Y-m-d\TH:i:sP', strtotime($article['date_modified'])) . '</lastmod>';
+					$output .= '<lastmod>' . date('Y-m-d\TH:i:sP', strtotime(($article['date_modified'] && $article['date_modified'] > $article['date_added'] ? $article['date_modified'] : $article['date_added']))) . '</lastmod>';
 					$output .= '<priority>1.0</priority>';
 					$output .= '<image:image>';
 					$output .= '<image:loc>' . $this->model_tool_image->resize($article['image'], $this->config->get($this->config->get('config_theme') . '_image_popup_width'), $this->config->get($this->config->get('config_theme') . '_image_popup_height')) . '</image:loc>';
@@ -65,7 +65,7 @@ class ControllerExtensionFeedBlogSitemap extends Controller {
 			$output .= '<url>';
 			$output .= '<loc>' . $this->url->link('blog/category', 'blog_category_id=' . $new_path) . '</loc>';
 			$output .= '<changefreq>weekly</changefreq>';
-			$output .= '<lastmod>' . date('Y-m-d\TH:i:sP', strtotime($result['date_modified'])) . '</lastmod>';
+			$output .= '<lastmod>' . date('Y-m-d\TH:i:sP', strtotime(($result['date_modified'] && $result['date_modified'] > $result['date_added'] ? $result['date_modified'] : $result['date_added']))) . '</lastmod>';
 			$output .= '<priority>0.7</priority>';
 			$output .= '</url>';
 
