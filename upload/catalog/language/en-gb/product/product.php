a9c21f897058a5ef100b9592237e9509abba45f8
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2025-06-22 20:33:24 +0300

    Update product.php

--- upload/catalog/language/en-gb/product/product.php
+++ upload/catalog/language/en-gb/product/product.php
@@ -1,5 +1,5 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2021.
+// *	@copyright	OPENCART.PRO 2011 - 2025.
 // *	@forum		https://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
@@ -9,6 +9,7 @@ $_['text_search']              = 'Search';
 $_['text_brand']               = 'Brand';
 $_['text_manufacturer']        = 'Brand:';
 $_['text_model']               = 'Product Code:';
+$_['text_sku']                 = 'SKU:';
 $_['text_reward']              = 'Reward Points:';
 $_['text_points']              = 'Price in reward points:';
 $_['text_stock']               = 'Availability:';
