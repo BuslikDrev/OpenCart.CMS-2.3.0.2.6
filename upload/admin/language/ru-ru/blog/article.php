bad707ad3d72ae9301f1d0eedb6db5986e1d4b2c
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2026-03-06 16:51:34 +0300

    Update article.php

--- upload/admin/language/ru-ru/blog/article.php
+++ upload/admin/language/ru-ru/blog/article.php
@@ -1,5 +1,5 @@
 <?php
-// *	@copyright	OPENCART.PRO 2011 - 2021.
+// *	@copyright	OPENCART.PRO 2011 - 2026.
 // *	@forum		https://forum.opencart.pro
 // *	@source		See SOURCE.txt for source and other copyright.
 // *	@license	GNU General Public License version 3; see LICENSE.txt
@@ -32,6 +32,7 @@ $_['entry_meta_keyword']     = 'Мета-тег Keywords';
 $_['entry_meta_description'] = 'Мета-тег Description';
 $_['entry_keyword']          = 'SEO URL';
 $_['entry_store']            = 'Магазины';
+$_['entry_author']           = 'Автор';
 $_['entry_image']            = 'Изображение статьи';
 $_['entry_status']           = 'Статус';
 $_['entry_noindex']          = 'Индексация';
